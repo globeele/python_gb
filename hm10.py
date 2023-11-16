fe39101cb8d280154c9b23796ea2003dc2efb35c
globeele <nsetko@yahoo.com>
2023-11-16 15:21:52 +0300

    hw 10 done

---
+++ hm10.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+df = pd.DataFrame({'whoAmI':lst})
+
+
+df.loc[df['whoAmI'] == 'robot', 'robot'] = '1'
+df.loc[df['whoAmI'] == 'human', 'robot'] = '0'
+df.loc[df['whoAmI'] == 'robot', 'human'] = '0'
+df.loc[df['whoAmI'] == 'human', 'human'] = '1'
+
+df.head()
